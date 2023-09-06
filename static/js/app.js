0c211697321f822c49b10679547c20a42aadd33c
Xiongp50 <131545109+Xiongp50@users.noreply.github.com>
2023-09-05 19:14:36 -0500

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,109 @@
+// Set up URL for data
+const url =
+    "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data){
+    console.log(data);
+});
+
+// Set up variables and get data from JSON for charts 
+var samples;
+var meta_data;
+d3.json(url).then(function (data) {
+    let selector = d3.select("#selDataset");
+    meta_data = data.metadata;
+    samples = data.samples;
+    data.names.forEach((id) => {
+        selector.append("option").text(id).property("value", id);
+    });
+    DemoData(meta_data[0]);
+    BarChart(samples[0]);
+    BubbleGraph(samples[0]);
+});
+
+function optionChanged(value) {
+    const selectedId = samples.find((item) => item.id === value);
+    const demographicInfo = meta_data.find((item) => item.id == value);
+
+    DemoData(demographicInfo);
+
+    BarChart(selectedId);
+
+    BubbleGraph(selectedId);
+
+}
+
+function DemoData(demographicInfo) {
+    let DemoDataSelect = d3.select("#sample-metadata");
+
+    DemoDataSelect.html(
+    `id: ${demographicInfo.id} <br> 
+    ethnicity: ${demographicInfo.ethnicity} <br>
+    gender: ${demographicInfo.gender} <br>
+    age: ${demographicInfo.age} <br>
+    location: ${demographicInfo.location} <br>
+    bbtype: ${demographicInfo.bbtype} <br>
+    wfreq: ${demographicInfo.wfreq}`
+    );
+}
+
+function BarChart(selectedId) {
+    let x_axis = selectedId.sample_values.slice(0, 10).reverse();
+    let y_axis = selectedId.otu_ids
+        .slice(0, 10)
+        .reverse()
+        .map((item) => `OTU ${item}`);
+    let text = selectedId.otu_labels.slice(0, 10).reverse();
+
+    bar = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        type: "bar",
+        orientation: "h",
+    };
+
+    let chart = [bar];
+
+    let layout = {
+        margin: {
+            l: 100,
+            r: 100,
+            t: 0,
+            b: 100,
+        },
+        height: 500,
+        width: 600,
+    };
+
+    Plotly.newPlot("bar", chart, layout);
+}
+
+function BubbleGraph(ID) {
+    let x_axis = ID.otu_ids;
+    let y_axis = ID.sample_values;
+    let marker_size = ID.sample_values;
+    let color = ID.otu_ids;
+    let text = ID.otu_labels;
+
+    bubble = {
+        x: x_axis,
+        y: y_axis,
+        text: text,
+        mode: "markers",
+        marker: {
+            color: color,
+            size: marker_size,
+        },
+        type: "scatter",
+    };
+    let chart = [bubble];
+
+    let layout = {
+        xaxis: {
+            title: { text: "OTU ID" },
+        },
+    };
+    Plotly.newPlot("bubble", chart, layout);
+}
